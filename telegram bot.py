eb27b52504c0bc57f16c7c47a48704bf7e4e4d98
RustamovOlmasjon <olmasjonrustamov1@gmail.com>
2025-11-01 14:42:29 +0500

    Add files via upload

---
+++ telegram bot.py
@@ -0,0 +1,9 @@
+# ism = "elyorbek"
+#
+# yosh = 17
+#
+# print(f"Assalomu aleykum {ism}sizning yoshingiz {yosh} sizning yoshingiz 10-yildan keyin {yosh+10}")
+
+
+son = int(input("son kiriting: " ))
+if son  == 2 :
