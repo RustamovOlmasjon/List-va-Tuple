c0a86536b38e67d4e78270d3a14d87a59a8874fd
RustamovOlmasjon <olmasjonrustamov1@gmail.com>
2025-11-01 14:39:56 +0500

    Add files via upload

---
+++ 30.10.2025.py
@@ -0,0 +1,666 @@
+# fruits = ['olma', 'banan','uzum']
+# fruits.append('nok')
+#
+# print(fruits)
+#
+# olma = len(fruits)
+# print(olma)
+#
+# if "olma" in fruits:
+#     print("true")
+# else:
+#     print('false')
+#
+# banan = fruits.index("banan")
+# print(banan)
+#
+# print(fruits,olma,banan)
+#
+
+
+
+# 2- masla
+
+# colors = ["qizil', 'yashil', 'alo',ko'k"]
+#
+# colors.append('sariq')
+# print(colors)
+#
+# colors.sort()
+# print(colors)
+#
+#
+# uzun = len(colors)
+# print(uzun)
+#
+# if 'yashil' in colors:
+#     print('true')
+# else:
+#     print('false')
+
+    # 3 - masla
+
+# cities = ['toshkent','xorazm','navoi']
+#
+# cities.append('xiva')
+#
+#
+# royhat = cities.index('xorazm')
+# print(royhat)
+#
+#
+# uzun = len(cities)
+# print(uzun)
+#
+#
+# cities.sort()
+# print(cities)
+
+# 4- masala
+
+#
+# sonlar = [5,3,1,8,7]
+#
+# sonlar.append('10')
+# print(sonlar)
+#
+# # sonlar.sort()
+# # print(sonlar)
+#
+# uzn= len(sonlar)
+# print(uzn)
+#
+# if '8' in sonlar:
+#     print('true')
+# else:
+#     print('folse')
+
+    # 5 - maslaa
+
+# hayvonlar = ['pishik','it','qush']
+#
+# hayvonlar.append('baliq')
+# print(hayvonlar)
+#
+# hayvonlar.sort()
+# print(hayvonlar)
+#
+# b =   hayvonlar.index('it')
+# print(b)
+#
+# a = len(hayvonlar)
+# print(a)
+
+
+# 6 - masal
+
+
+# meva = ['olma', 'banan', 'uzum', 'nok']
+#
+# meva.append('kivi')
+# print(meva)
+#
+# meva.remove('banan')
+# print(meva)
+#
+# meva.insert(2,'shaftoli')
+# print(meva)
+#
+# meva.sort()
+# print(meva)
+#
+# a = len(meva)
+# print(a)
+#
+# a = meva.index('olma')
+# print(a)
+#
+# if 'kivi' in meva:
+#     print('true')
+# else:
+#     print('folse')
+#
+#
+# meva.sort(reverse=True)
+# print(meva)
+#
+#
+# a = meva.copy()
+# print(a)
+#
+#
+# a.append('ananas')
+# print(a)
+
+# 7 - masala
+#
+# menu = ['osh', "lagmon", 'shashlik', 'somsa']
+#
+# menu.extend('manti' 'norin')
+#
+# print(menu)
+#
+# menu.remove('lagmon')
+# print(menu)
+#
+# menu.sort()
+# print(menu)
+#
+# menu.pop()
+# print(menu)
+#
+# a = len(menu)
+# print(a)
+#
+# a = menu.index('osh')
+# print(a)
+#
+# menu.count('somsa')
+# print(menu)
+#
+#
+# if 'manti' in menu:
+#     print('true')
+# else:
+#     print('folse')
+#
+# a = menu.copy()
+# print(a)
+#
+# a.append('qozon kovob')
+#
+# print(a)
+#
+
+
+# # 8 - masala
+#
+# cities = ['Toshkent', 'Samarqand', 'Buxoro', 'Xiva']
+#
+#
+# cities.insert(0,'Andijon')
+# print(cities)
+#
+# cities.remove('Xiva')
+# print(cities)
+#
+# a = cities.index('Samarqand')
+# print(a)
+#
+# cities.sort()
+# print(cities)
+#
+# a = len(cities)
+# print(a)
+#
+# if 'buxoro' in cities:
+#     print('true')
+# else:
+#     print('folse')
+#
+# cities.pop()
+# print(cities)
+#
+# cities.sort(reverse=True)
+# print(cities)
+#
+# a = cities.copy()
+# print(a)
+#
+# a.append('Fargona')
+# print(a)
+
+
+# 9 - maslaa
+#
+# students = ['Ali', 'Vali', 'Sardor', 'Aziz']
+#
+# students.append('jamshid')
+# print(students)
+#
+# students.remove('Sardor')
+# print(students)
+#
+# students.insert(0,'diyor')
+# print(students)
+#
+# if 'diyor' in students:
+#     print("true")
+# else:
+#     print('folse')
+#
+#
+# students.pop()
+# print(students)
+#
+# a = len(students)
+# print(a)
+#
+# a = students.copy()
+# print(a)
+#
+# a.append('nodir')
+# print(a)
+
+
+# 10 - masla
+
+# favorites = ['telefon', 'noutbuk', 'naushnik']
+#
+# favorites.append('planshet')
+# print(favorites)
+#
+# favorites.append('saot')
+# print(favorites)
+#
+# favorites.sort()
+# print(favorites)
+#
+# a = len(favorites)
+# print(a)
+#
+# a = favorites.index('noutbuk')
+# print(a)
+#
+# if 'telefon' in favorites:
+#     print('true')
+# else:
+#     print('folse')
+#
+# favorites.remove('naushnik')
+# print(favorites)
+#
+# favorites.pop()
+# print(favorites)
+#
+# a = favorites.copy()
+# print(a)
+#
+# a.append('tilivizor')
+# print(a)
+
+
+# 11- masala
+
+# songs = ['Yomgir', 'Sevgi', 'Hayot', 'Kuz']
+#
+# songs.insert(0,'bahor')
+# print(songs)
+#
+# songs.remove('Kuz')
+# print(songs)
+#
+# songs.sort()
+# print(songs)
+#
+# a = songs.index('Yomgir')
+# print(a)
+#
+# if 'Hayot' in songs:
+#     print('true')
+# else:
+#     print('folse')
+#
+# songs.pop()
+# print(songs)
+#
+# a = len(songs)
+# print(a)
+#
+# songs.sort(reverse=True)
+# print(songs)
+#
+# a = songs.copy()
+# print(a)
+#
+# a.append('yoz')
+# print(a)
+#
+
+
+
+# 12 - masala
+#
+# books = ['Alximik', 'Shaytanat', '1984']
+#
+#
+# books.extend('sariq devni minib ' 'otkan kunlar')
+# print(books)
+#
+# books.remove('Shaytanat')
+# print(books)
+#
+# books.sort()
+# print(books)
+#
+# a = books.index('1984')
+# print(a)
+#
+# if 'Alximik' in books:
+#     print('Turue')
+# else:
+#     print('folse')
+#
+# books.pop()
+# print(books)
+#
+# a = len(books)
+# print(a)
+#
+# books.sort(reverse=True)
+# print(books)
+#
+# a = books.copy()
+# print(a)
+#
+# a.append('Qirq yil')
+# print(a)
+#
+
+
+# 13 - masala
+
+# movies = ['Inception', 'Matrix', 'Interstellar']
+#
+# movies.append('parasite')
+# print(movies)
+#
+# movies.remove('Matrix')
+# print(movies)
+#
+# movies.sort()
+# print(movies)
+#
+# a = movies.index('Inception')
+# print(a)
+#
+# if 'Interstellar' in movies:
+#     print('Turue')
+# else:
+#     print('folse')
+#
+# movies.pop()
+# print(movies)
+#
+# a = len(movies)
+# print(a)
+#
+# movies.sort(reverse=True)
+# print(movies)
+#
+# a = movies.copy()
+# print(a)
+#
+# a.append('Joker')
+# print(a)
+
+
+
+# 14 - masala
+
+# emails = ['job_offer', 'spam', 'friend', 'news']
+#
+# emails.append('familiy')
+# print(emails)
+#
+# emails.remove('spam')
+# print(emails)
+#
+# emails.sort()
+# print(emails)
+#
+# a = emails.index('friend')
+# print(a)
+#
+# if 'job_offer' in emails:
+#     print('true')
+# else:
+#     print('folse')
+#
+# emails.pop()
+# print(emails)
+#
+# a = len(emails)
+# print(a)
+#
+# emails.sort(reverse=True)
+# print(emails)
+#
+# a = emails.copy()
+# print(a)
+#
+# a.append('urget')
+# print(a)
+
+
+# 15 - masala
+
+# friends = ['Ali', 'Zara', 'Bobur', 'Nodir']
+#
+# friends.append('jamol')
+# print(friends)
+#
+# friends.remove('Nodir')
+# print(friends)
+#
+# friends.sort()
+# print(friends)
+#
+# a = friends.index('Zara')
+# print(a)
+#
+# if 'Ali' in friends:
+#     print('turue')
+# else:
+#     print('folse')
+#
+# friends.pop()
+# print(friends)
+#
+# a = len(friends)
+# print(a)
+#
+# friends.sort(reverse=True)
+# print(friends)
+#
+# a = friends.copy()
+# print(a)
+#
+# a.append('Sardor')
+# print(a)
+#
+
+
+# 16 - masala
+#
+# animals = ['sher', 'yolbars', 'ayiq', 'sher', 'quyon']
+#
+# animals.count('sher')
+# print(animals)
+#
+# animals.remove('ayiq')
+# print(animals)
+#
+# animals.append('kenguru')
+# print(animals)
+#
+# animals.sort()
+# print(animals)
+#
+# a = animals.index('yolbars')
+# print(a)
+#
+# if 'quyon' in animals:
+#     print('turue')
+# else:
+#     print('folse')
+#
+# animals.pop()
+# print(animals)
+#
+# a = len(animals)
+# print(a)
+#
+# a = animals.copy()
+# print(a)
+#
+# a.append('tuya')
+# print(a)
+
+# 17 - masala
+
+
+# champions = ['Khamzat Chimaev', 'Khabib Nurmagomedov', 'Conor McGregor', 'Mike Tyson', 'Wladimir Klichko', 'Eliud']
+#
+# champions.count('Mike Tyson')
+# print(champions)
+#
+# champions.pop()
+# print(champions)
+#
+# champions.extend('baxodir jalalov''hasanboy dostmatov')
+# print(champions)
+#
+# champions.sort()
+# print(champions)
+#
+# a = champions.index('Mike Tyson')
+# print(a)
+#
+# if 'Khabib Nurmagomedov' in champions:
+#     print('true')
+# else:
+#     print('folse')
+#
+# champions.remove('Conor McGregor')
+# print(champions)
+#
+# a = len(champions)
+# print(a)
+#
+# a = champions.copy()
+# print(a)
+#
+# a.append('Shaxram Giyasov')
+# print(a)
+
+
+# 18 - masala
+
+
+# cart = ['non', 'suyut', 'tuxum', "go'sht"]
+#
+# a = cart.index('tuxum')
+# print(a)
+#
+# cart.remove('suyut')
+# print(cart)
+#
+# cart.extend('sariq yog''murobo')
+# print(cart)
+#
+# cart.sort()
+# print(cart)
+#
+# a = cart.copy()
+# print(a)
+#
+# a.append('shakar')
+# print(a)
+#
+# print('diqat bu hamaa molumotlarni ochiradi ')
+# cart.clear()
+# print(cart)
+#
+# if 'non' in cart:
+#     print('true')
+# else:
+#     print('folse')
+#
+# a = len(cart)
+# print(a)
+#
+# cart.pop()
+# print(cart)
+
+
+# 19 - masala
+# courses = ['Python', 'Java', 'Data Science', 'Python']
+#
+# courses.count('Python')
+# print(courses)
+#
+# courses.remove('Java')
+# print(courses)
+#
+# courses.extend('Al''web')
+# print(courses)
+#
+# courses.sort()
+# print(courses)
+#
+# a = courses.index('Data Science')
+# print(a)
+#
+# if 'Al' in courses:
+#     print('true')
+# else:
+#     print('folse')
+#
+# courses.pop()
+# print(courses)
+#
+# courses.remove('Python')
+# print(courses)
+#
+# a = len(courses)
+# print(a)
+#
+# a = courses.copy()
+# print(a)
+#
+# a.append('Blockchain')
+# print(a)
+#
+
+# 20  - masala
+
+cars = ['Chevrolet', 'Hyundai', 'Kia', 'Toyota']
+
+cars.insert(0,'BMW')
+print(cars)
+
+cars.remove('Kia')
+print(cars)
+
+a = cars.copy()
+print(a)
+
+a.append('mersides')
+print(a)
+
+cars.sort()
+print(cars)
+
+a = cars.index('Toyota')
+print(a)
+
+if 'Hyundai' in cars:
+    print('turue')
+else:
+    print('folse')
+
+cars.pop()
+print(cars)
+
+a = len(cars)
+print(a)
+
+cars.sort(reverse=True)
+print(cars)
+
+print('hamma molumotni ociradi')
+
+cars.clear()
+print(cars)
+
