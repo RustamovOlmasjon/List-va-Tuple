1cca86a99d670a7f046ef2769040d721eae9ce7b
RustamovOlmasjon <olmasjonrustamov1@gmail.com>
2025-11-01 14:44:26 +0500

    Add files via upload

---
+++ 11102025.py
@@ -0,0 +1,124 @@
+# # soz = input("sozni kiriting ").lower()
+# # soz = input("sozni kiriting ").lower().if soz ==[::-1]:.print("palimdrom soz ").else:.print("palimdrom emas ")
+# #
+# #
+# #
+# #202
+# #202
+# #202
+# #
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+# # oy = int(input('oy raqamini kiriting'))
+# # harorat = int(input("haroratni kiriting (masalan 20.5)"))
+# # if oy in [12,1,2]:
+# #     print("qish ")
+# # elif oy in [3,4,5,]:
+# #     if harorat > 15:
+# #         print("iliq bahor ")
+# #     else:
+# #         print("sovuq baxor ")
+# # elif  oy in [6,7,8,]:
+# #      print("yoz")
+# # elif oy in [9,10,11 ]:
+# #     print("kuz")
+# # else:
+# #     print("qiymat hato kiritildi ")
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+# # yil = int(input("yilni kiriting "))
+# # if (yil % 400 == 0 ) or ( yil%4 ==0  and yil % 100 != 0 ):
+# #     print("yil kabisa yili ")
+# # else:
+# #     print("yil kbisa yili emas ")
+# #
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+# jihoz = int( input(" jihozlar sonini kirting "))
+# vaht = int(input("vahtni kiriting "))
+# if jihoz < 5 or vaht< 8:
+#     print("tejamkor ")
+# elif jihoz< 5 or vaht >= 8:
+#     print("ortaca istemol ")
+# elif jihoz>= 5 or vaht < 8:
+#     print("kop istemol ")
+# elif jihoz >= 5 or vaht>= 8 :
+#     print("haddan tawqari kop")
+# else:
+#     print("banqrot ")
+#
+#
+
+#
+# qahva = int(input("qahva isiqlikini kiriting "))
+# if qahva >= 60:
+#     print("ogoh boling ")
+# elif qahva > 30 - 60 :
+#     print("idial ")
+# elif qahva <30 :
+#     print("sovib qolgan ")
+
+
+
+
+# zarat = int(input(' zaratni kiriting '))
+# if zarat <10 :
+#     print("zarat zarur ")
+# elif zarat > 10 - 50 :
+#     print("ishaltiw mumkun ")
+# elif zarat > 50 :
+#     print("yetarli" )
+# else:
+#     print("ocgan ")
+
+
+
+raqam_tek = int(input("raqmni kiriting" ))
+if raqam_tek == 91:
+    print("belain ")
+elif raqam_tek== 93:
+    print("usel ")
+elif raqam_tek== 97:
+    print("mobi uz ")
+
